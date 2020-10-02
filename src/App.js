85974e6faeee5263adb147cb0237b8b9b8ecfe58
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-10-02 14:32:26 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
+import BabyNamesData from "./babyNamesData.json"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function NameList(props) {
+    return (
+        <div id="nameList">
+            <ul>
+                {props.names.map(name => (
+                    <li id={name.id} class={name.sex}>{name.name}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
+
+function App() {
+    return (
+        <div>
+            <NameList names={BabyNamesData.sort((a, b) => a.name > b.name)} />
+        </div>
+    );
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
