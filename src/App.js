a9569a217d57c25154d754b30abc61ee7726ea8e
iggajewski <64140239+iggajewski@users.noreply.github.com>
2020-10-02 15:29:08 +0100

    lvl 2 finished

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
 import React from 'react';
-import BabyNamesData from "./babyNamesData.json"
+import {useState} from 'react';
 import './App.css';
 
+import BabyNamesData from "./babyNamesData.json"
+BabyNamesData.sort((a, b) => a.name > b.name);
+
 function NameList(props) {
-    return (
-        <div id="nameList">
-            <ul>
-                {props.names.map(name => (
-                    <li id={name.id} class={name.sex}>{name.name}</li>
-                ))}
-            </ul>
-        </div>
+    return (    
+        <ul>
+            {props.names.map(
+                name => (<li id={name.id} class={name.sex}>{name.name}</li>
+            ))}
+        </ul>
     );
 }
 
 function App() {
+    let [nameList, setNameList] = useState(BabyNamesData);
+
+    function findNames() {
+        let result = document.getElementById("searchBar").value.toLowerCase();
+        
+        if(result === "") {
+            return setNameList(nameList = BabyNamesData);
+        }
+          
+        let newNameList = [];          
+        for (let i = 0; i < BabyNamesData.length; ++i) {             
+            if(BabyNamesData[i].name.toLowerCase().includes(result)) {                   
+                newNameList.push(BabyNamesData[i]);              
+            }           
+        }     
+        return setNameList(nameList = newNameList);
+    }
+
     return (
-        <div>
-            <NameList names={BabyNamesData.sort((a, b) => a.name > b.name)} />
+        <div id="nameList">
+            <input id="searchBar" onInput={findNames}></input>
+            <NameList names={nameList} />
+            <hr id="line"/>
         </div>
     );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
